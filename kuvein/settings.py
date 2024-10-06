8d1c45319097718d26496fce4e96c2119f278c90
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-10-06 20:38:20 +0700

    Added .env, requirement.txt and made the app more secure with python-decouple

--- kuvein/settings.py
+++ kuvein/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p)s7-+)_mpla*0+r3pi42cgztz89uh@jgo!s41k%f&++!5i2lo'
+SECRET_KEY = config('SECRET_KEY', cast=str,
+                    default='bananaappleorangemonkeymoney\
+                        noodleenglishorspanish')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',
+                       cast=lambda v: [s.strip() for s in v.split(',')],
+                       default='*')
 
 
 # Application definition
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = config('TIME_ZONE', cast=str, default='UTC')
 
 USE_I18N = True
 
