9608d20434ca9767dc5eae86a6e8ce8905e82da5
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-09 00:22:10 +0700

    refactor: Use flake8 and enum to improve readability

--- Backend/forms/info_loader.py
+++ Backend/forms/info_loader.py
@@ -8,9 +8,9 @@ import json
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-def get_url():
-    """GET all the course data url"""
 
+def get_url() -> list[str]:
+    """GET all the course data url."""
     # link change each year to make it uptodate
     cur_year = datetime.now().year
     url = f'https://registrar.ku.ac.th/class#{cur_year + 543}'
@@ -45,9 +45,8 @@ def get_url():
         print(f"Failed to retrieve page. Status code: {response.status_code}")
 
 
-def download_pdf(url, filename):
-    """Function to download PDF files."""
-
+def download_pdf(url: str, filename: str):
+    """Download PDF files."""
     response = requests.get(url)
     if response.status_code == 200:
         with open(filename, 'wb') as f:
@@ -57,8 +56,9 @@ def download_pdf(url, filename):
         print(f"Failed to download {filename}\n")
 
 
-def extract_text_from_pdf(pdf_path, filename):
-    """Function to extract text from the PDF using pdfplumber."""
+def extract_text_from_pdf(pdf_path: str,
+                          filename: str) -> dict[str, dict[str, str]]:
+    """Extract text from the PDF using pdfplumber."""
     print(f"Start Extract Text From {filename}\n")
     with pdfplumber.open(pdf_path) as pdf:
         faculty = ""
@@ -69,7 +69,7 @@ def extract_text_from_pdf(pdf_path, filename):
             page_text = page.extract_text()  # Extract text from the page
 
             if page_text:  # Check if the page contains text
-                lines = page_text.split('\n')  # Split the page text into individual lines
+                lines = page_text.split('\n')
 
                 # Inner loop. Loop each line of the page.
                 for line in lines:
@@ -79,27 +79,36 @@ def extract_text_from_pdf(pdf_path, filename):
                         faculty = cur_line[0]
                         try:
                             if result_data[faculty]:
-                                print(f"Continue Extract Data From {filename}...\n")
+                                print(f"Continue Extract Data From"
+                                      f" {filename}...\n")
 
                         except KeyError:
                             result_data[faculty] = {}
 
                     # This code might need to update to 02 in the future.
-                    # However, I hope the teacher will change data from pdf into html
+                    # However, I hope the teacher will
+                    # change data from pdf into html
 
                     elif cur_line[0][:2] == "01" and faculty:
                         # Filtering criteria for each line
                         for i in range(len(cur_line)):
                             # In line element loop
-                            if cur_line[i][0].isnumeric() and i not in [0, 1] and len(cur_line[0]) >= 3:
+                            if cur_line[i][0].isnumeric() and \
+                                    i not in [0, 1] and len(cur_line[0]) >= 3:
                                 # Check when the name of the course end
 
-                                course_id = cur_line[:1][0] # Add the filtered line to the final text
+                                course_id = cur_line[:1][0]
                                 course_name = " ".join(cur_line[2:i])
 
-                                if not "Thesis" in course_name.split() and "Seminar" != course_name \
+                                if "Thesis" not in course_name.split() \
+                                        and "Seminar" != course_name \
                                         and "(ต่อ)" != course_name:
-                                    result_data[faculty][course_id] = course_name
+
+                                    # Seriously
+                                    # Flake8?
+                                    result_data[faculty][
+                                        course_id
+                                    ] = course_name
 
                                 break
 
@@ -107,48 +116,43 @@ def extract_text_from_pdf(pdf_path, filename):
         return result_data  # Return the filtered text
 
 
-def save_to_json(data, filename, dir_name):
-    """Function to save data as JSON."""
-
+def save_to_json(data: dict[str, dict[str, str]],
+                 filename: str, dir_name: str):
+    """Save data as JSON."""
     with open(f'{dir_name}/{filename}', 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
     print(f"Data saved to {dir_name}/{filename}")
 
+
 def create_data():
-    """Function to combine all function in this module."""
+    """Generate all data."""
     pdf_urls = get_url()
     directory_path = "./database"
 
     # Loop through each PDF URL, download, and extract text
-    set_filename1 = ["normal2", "special2", "inter2", "normal1", "special1", "inter1"]
-    set_filename2 = ["normal1", "special1", "inter1"]
+    set_filenames = ["normal2", "special2",
+                     "inter2", "normal1", "special1", "inter1"]
+    if len(pdf_urls) == 3:
+        set_filenames = set_filenames[3:]
 
-    if len(pdf_urls) == 6:
-        set_filename = set_filename1
-    else:
-        set_filename = set_filename2
+    for index, url in enumerate(pdf_urls):
+        try:
+            # Extract the filename from the URL (last part after the slash)
+            pdf_filename = url.split('/')[-1]
 
-    index = 0
+            # Download the PDF file
+            download_pdf(url, pdf_filename)
 
-    for url in pdf_urls:
-        # Extract the filename from the URL (last part after the slash)
-        name_of_the_file = url.split('/')[-1]
+            # Extract text from the PDF
+            extracted_text = extract_text_from_pdf(pdf_filename,
+                                                   set_filenames[index])
 
-        # Download the PDF file
-        download_pdf(url, name_of_the_file)
+            # Save extracted text to a JSON file
+            json_filename = f'{set_filenames[index]}.json'
+            save_to_json(extracted_text, json_filename, directory_path)
 
-        # Extract text from the PDF
-        extracted_text = extract_text_from_pdf(name_of_the_file, set_filename[index])
-
-        # Save all extracted PDF data to a JSON file
-        save_to_json(extracted_text, f'{set_filename[index]}.json', directory_path)
-
-        # Store the extracted text in a dictionary
-        index += 1
-
-        # Cleanup: Remove the downloaded PDF after processing
-        os.remove(name_of_the_file)
-
-
-
-create_data()
+        finally:
+            # Cleanup: Remove the downloaded PDF after processing
+            if os.path.exists(pdf_filename):
+                os.remove(pdf_filename)
+            print(f"Cleaned up: {pdf_filename}")
