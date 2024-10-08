e74f838d05ac804ae51d4dadb0085cedffc64bb7
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-10-08 20:46:11 +0700

    build: Added mysql config to settings.py

--- Backend/kuvein/settings.py
+++ Backend/kuvein/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'kuvein.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Course',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
