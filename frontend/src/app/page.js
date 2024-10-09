a14122cb9fb662bca531ae5dd9b6bfbed701c408
PanidaRumriankit <panida.rum@ku.th>
2024-10-10 01:35:26 +0700

    feat: usable dark theme

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-white">
+    <div className="flex flex-col items-center min-h-screen dark:bg-black">
       <main className="flex flex-col items-center pt-20">
         <Image
           src="/artery.png"
@@ -11,8 +11,8 @@ export default function Home() {
           height={120}
           priority
         />
-        <h1 className="mt-4 text-3xl font-bold text-black">KU Vein</h1>
-        <p className="mt-4 text-xl text-black">รีวิว แบ่งปัน Q&A</p>
+        <h1 className="mt-4 text-3xl font-bold dark:text-white">KU Vein</h1>
+        <p className="mt-4 text-xl dark:text-white">รีวิว แบ่งปัน Q&A</p>
       </main>
       <div className="mt-8 w-full max-w-6xl">
         <input
