aada64bacfb3402a1dd68a3b4fa1dd18974a4ce7
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-10-12 20:28:18 +0700

    fix: Reworked information scraping module and fixed name being lost in the scraping. Also removed subjects with empty names

--- Backend/forms/info_loader.py
+++ Backend/forms/info_loader.py
@@ -4,9 +4,13 @@ import requests
 import pdfplumber
 import os
 import json
+import re
 
 from bs4 import BeautifulSoup
 from datetime import datetime
+
+
+FORBIDDEN_WORD = ['Pre']
 
 
 def get_url() -> list[str]:
@@ -56,6 +60,96 @@ def download_pdf(url: str, filename: str):
         print(f"Failed to download {filename}\n")
 
 
+def is_thai(text: str):
+    """Checks if any character in the string is either numeric or thai"""
+    return any(re.match("^[\u0E00-\u0E7F]*$", t) for t in text)
+
+
+def is_subject_name(text: str):
+    """
+    The string is subject name if it passes these criteria
+    1. Each character is not in thai and not a number
+    2. string is not in upper case
+    3. string is not in FORBIDDEN_WORD
+    """
+    if any(is_thai(t) or t.isnumeric() for t in text):
+        return False
+    if text.isupper():
+        return False
+    if len(text) <= 1:
+        return False
+    if text in FORBIDDEN_WORD:
+        return False
+    return True
+
+
+def has_num_and_alpha(text: str):
+    """Check if the string has number and alphabet at the same time"""
+    return any(t.isnumeric() for t in text) and any(t.isalpha() for t in text)
+
+
+def is_valid_line(lines: list[str]):
+    """
+    Checks that the line is not gibberish and is either 
+    a normal subject line. Ex. '01999011 -64 Food for Mankind 3 3 1 ...' or
+    a subject name line. Ex. 'Sustainability'
+    """
+    first = lines[0]
+    if is_thai(first):
+        return False
+    if len(first) < 2:
+        return False
+    if has_num_and_alpha(first):
+        return False
+    if first.isnumeric():
+        if len(first) == 8:
+            return True
+        else:
+            return False
+    return True
+
+
+def get_last_subject_id(fac: str):
+    """Get the last added subject's id"""
+    global RESULT_DATA
+    return list(RESULT_DATA[fac].keys())[-1]
+
+
+def handle_line(text: list[str], fac: str):
+    """
+    Handle each line and will either create a new subject in result_data or
+    add to the previous subject's name
+    """
+    global RESULT_DATA
+    if all(t.isalpha() and not is_thai(t) for t in text[0]):
+        subject_name = get_subject_name(text, 0)
+        RESULT_DATA[fac][get_last_subject_id(fac)] += subject_name
+        return
+    if not is_valid_line(text):
+        return
+    subject_id = text[0]
+    if len(subject_id) != 8:
+        return
+    subject_name = get_subject_name(text, 2)
+    if len(subject_name) == 0:
+        return
+    RESULT_DATA[fac][subject_id] = subject_name
+
+
+def get_subject_name(text: list[str], index: int = 0):
+    """
+    Loop through the list of string 
+    until the string fails is_subject_name() function
+    """
+    subject_name = ''
+    while is_subject_name(text[index]):
+        subject_name += ' '+text[index]
+        index += 1
+        if index >= len(text):
+            break
+    return subject_name
+
+
 def extract_text_from_pdf(pdf_path: str,
                           filename: str) -> dict[str, dict[str, str]]:
     """Extract text from the PDF using pdfplumber."""
@@ -63,64 +157,36 @@ def extract_text_from_pdf(pdf_path: str,
     with pdfplumber.open(pdf_path) as pdf:
         group_id = ""
         faculty = "undefined"
-        result_data = {faculty: {}}
-
+        global RESULT_DATA
+        RESULT_DATA[faculty] = {}
         # Loop PDF pages
-        for page in pdf.pages:
+        for page in pdf.pages[:5]:
             page_text = page.extract_text()  # Extract text from the page
 
-            if page_text:  # Check if the page contains text
-                lines = page_text.split('\n')
+            if not page_text:  # Check if the page contains text
+                continue
+            lines = page_text.split('\n')
 
-                # Inner loop. Loop each line of the page.
-                for line in lines:
-                    cur_line = line.split()
+            # Inner loop. Loop each line of the page.
+            for line in lines:
+                cur_line = line.split()
 
-                    if "รหัสวิชา" in line:
-                        group_id = line.split()[1]
+                if "รหัสวิชา" in line:
+                    group_id = line.split()[1]
 
-                    elif "คณะ" == cur_line[0][:3]:
-                        faculty = cur_line[0]
-                        try:
-                            if result_data[faculty]:
-                                print(f"Continue Extract Data From"
-                                      f" {filename}...\n")
+                elif "คณะ" == cur_line[0][:3]:
+                    faculty = cur_line[0]
+                    try:
+                        if RESULT_DATA[faculty]:
+                            print(f"Continue Extract Data From"
+                                  f" {filename}...\n")
 
-                        except KeyError:
-                            result_data[faculty] = {}
+                    except KeyError:
+                        RESULT_DATA[faculty] = {}
 
-                    # This code might need to update to 02 in the future.
-                    # However, I hope the teacher will
-                    # change data from pdf into html
-
-                    elif (cur_line[0][:2] == "01" or
-                            cur_line[0][:2] in group_id):
-
-                        # Filtering criteria for each line
-                        for i in range(len(cur_line)):
-                            # In line element loop
-                            if cur_line[i][0].isnumeric() and \
-                                    i not in [0, 1] and len(cur_line[0]) >= 3:
-                                # Check when the name of the course end
-
-                                course_id = cur_line[:1][0]
-                                course_name = " ".join(cur_line[2:i])
-
-                                if "Thesis" not in course_name.split() \
-                                        and "Seminar" != course_name \
-                                        and "(ต่อ)" != course_name \
-                                        and course_name:
-
-                                    # Seriously
-                                    # Flake8?
-                                    result_data[faculty][
-                                        course_id
-                                    ] = course_name
-
-                                break
-
+                else:
+                    handle_line(cur_line, faculty)
         print("Successfully Combined Data\n")
-        return result_data  # Return the filtered text
 
 
 def save_to_json(data: dict[str, dict[str, str]],
@@ -133,8 +199,11 @@ def save_to_json(data: dict[str, dict[str, str]],
 
 def create_data():
     """Generate all data."""
+    global RESULT_DATA
     pdf_urls = get_url()
-    directory_path = "./database"
+    BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+    directory_path = BASE_PATH + "/database"
 
     # Loop through each PDF URL, download, and extract text
     set_filenames = ["normal2", "special2",
@@ -151,12 +220,12 @@ def create_data():
             download_pdf(url, pdf_filename)
 
             # Extract text from the PDF
-            extracted_text = extract_text_from_pdf(pdf_filename,
-                                                   set_filenames[index])
+            extract_text_from_pdf(pdf_filename,
+                                  set_filenames[index])
 
             # Save extracted text to a JSON file
             json_filename = f'{set_filenames[index]}.json'
-            save_to_json(extracted_text, json_filename, directory_path)
+            save_to_json(RESULT_DATA, json_filename, directory_path)
 
         finally:
             # Cleanup: Remove the downloaded PDF after processing
@@ -164,4 +233,6 @@ def create_data():
                 os.remove(pdf_filename)
             print(f"Cleaned up: {pdf_filename}\n")
 
+
+RESULT_DATA = {}
 create_data()
