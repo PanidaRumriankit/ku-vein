b9f191a1db1143e607b8c9114804a943486be003
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-14 17:22:00 +0700

    feat: Initialize table in MySQL server

--- backend/forms/dbm.py
+++ backend/forms/dbm.py
@@ -1,9 +1,86 @@
 import json
+import pymysql
+
+from decouple import config
 
 class DatabaseManagement:
     def __init__(self):
         self.data = None
+        self.connection = None
+        self.cursor = None
+
+        self.table_name = ['BookMark', 'QA', 'Summary', 'CourseReview', 'UserData', 'ReviewStat', 'CourseData']
         self.exist_data_loader()
+
+    def connect_mysql_server(self):
+        """Connect to MySQL server."""
+        timeout = 10
+        self.connection = pymysql.connect(
+            charset="utf8mb4",
+            connect_timeout=timeout,
+            cursorclass=pymysql.cursors.DictCursor,
+            db=config('MYSQLDATABASE', cast=str,
+                      default='Nergigante'),
+            host=config('MYSQLHOST', cast=str,
+                        default='Yes Indeed'),
+            password=config('MYSQLPASS', cast=str,
+                            default='do_not_give_up_skeleton'),
+            read_timeout=timeout,
+            port=22924,
+            user=config('MYSQLUSER', cast=str,
+                        default='praise_the_sun'),
+            write_timeout=timeout,
+        )
+
+        self.cursor = self.connection.cursor()
+
+    def table_initialize(self):
+        """Create table in MySQL server."""
+
+        self.connect_mysql_server()
+
+        try:
+
+            self.drop_all_table()
+            self.cursor.execute("CREATE TABLE CourseData(course_id INT, faculty VARCHAR(100),"
+                           " course_name LONGTEXT, PRIMARY KEY (course_id, faculty))")
+
+            self.cursor.execute("CREATE TABLE ReviewStat(review_id INT, course_id INT, "
+                           "date_data DATE, grade CHAR(1), upvotes INT, "
+                           "PRIMARY KEY (review_id), "
+                           "FOREIGN KEY (course_id) REFERENCES CourseData(course_id) ON DELETE CASCADE)")
+
+            self.cursor.execute("CREATE TABLE UserData(user_id INT UNIQUE, user_name VARCHAR(30) UNIQUE, "
+                           "user_type VARCHAR(20), email LONGTEXT, PRIMARY KEY (user_id))")
+
+            self.cursor.execute("CREATE TABLE CourseReview(review_id INT UNIQUE, user_id INT, course_id INT,"
+                           " reviews LONGTEXT, PRIMARY KEY (user_id, course_id),"
+                           " FOREIGN KEY (user_id) REFERENCES UserData(user_id) ON DELETE CASCADE,"
+                           " FOREIGN KEY (course_id) REFERENCES CourseData(course_id) ON DELETE CASCADE)")
+
+            self.cursor.execute("CREATE TABLE Summary(course_id INT, user_id INT, sum_text LONGTEXT,"
+                                " PRIMARY KEY (user_id, course_id),"
+                                " FOREIGN KEY (user_id) REFERENCES UserData(user_id) ON DELETE CASCADE,"
+                                " FOREIGN KEY (course_id) REFERENCES CourseData(course_id) ON DELETE CASCADE)")
+
+            self.cursor.execute("CREATE TABLE QA(question_id INT UNIQUE, user_id INT, comment LONGTEXT,"
+                                " PRIMARY KEY (question_id, user_id),"
+                                " FOREIGN KEY (user_id) REFERENCES UserData(user_id) ON DELETE CASCADE)")
+
+            self.cursor.execute("CREATE TABLE BookMark(review_id INT, user_id INT,"
+                                " PRIMARY KEY (review_id, user_id),"
+                                " FOREIGN KEY (review_id) REFERENCES CourseReview(review_id) ON DELETE CASCADE,"
+                                " FOREIGN KEY (user_id) REFERENCES UserData(user_id) ON DELETE NO ACTION)")
+
+
+        finally:
+            self.connection.close()
+
+    def drop_all_table(self):
+        """Drop every table in the database."""
+        for drop in self.table_name:
+            self.cursor.execute(f"DROP TABLE IF EXISTS {drop}")
+
 
     def exist_data_loader(self):
         """Combined all the data and separate by course programs."""
@@ -31,3 +108,5 @@ class DatabaseManagement:
 
 
 d = DatabaseManagement()
+
+d.table_initialize()
