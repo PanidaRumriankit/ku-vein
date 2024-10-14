92102433f7a1541616d02ab7d7c0efa07285bc6c
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-10-14 20:23:44 +0700

    chore: change report button from text to a flag icon

--- frontend/src/app/components/reportbutton.jsx
+++ frontend/src/app/components/reportbutton.jsx
@@ -8,13 +8,17 @@ import {
   Button
 } from "@nextui-org/react";
 import {useState} from "react";
+import OutlinedFlagTwoToneIcon from '@mui/icons-material/OutlinedFlagTwoTone';
+
 
 export default function ReportButton() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <Button onClick={() => setIsOpen(true)}
-              variant="light">รายงาน</Button>
+              variant="light">
+        <OutlinedFlagTwoToneIcon/>
+      </Button>
       <Modal size="sm" isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <ModalContent className="bg-gray-700">
           <ModalHeader className="text-green-500">Report</ModalHeader>
