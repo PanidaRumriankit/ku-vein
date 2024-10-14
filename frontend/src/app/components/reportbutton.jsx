8d25675038b757a3bb569eb84239d2a277251c7e
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-10-14 19:54:36 +0700

    feat: add a report button for the main page

---
+++ frontend/src/app/components/reportbutton.jsx
@@ -0,0 +1,35 @@
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Textarea,
+  Button
+} from "@nextui-org/react";
+import {useState} from "react";
+
+export default function ReportButton() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}
+              variant="light">รายงาน</Button>
+      <Modal size="sm" isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <ModalContent className="bg-gray-700">
+          <ModalHeader className="text-green-500">Report</ModalHeader>
+          <ModalBody>
+            <form>
+              <Textarea placeholder="เหตุผลในการรายงาน" autoFocus minRows={5}/>
+              {/* Add more form fields as needed */}
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={() => {/* Handle form submission */
+            }}>Submit</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,11 +3,12 @@
 import Image from "next/image";
 import {useState, useMemo} from "react";
 import {Button} from "@nextui-org/button";
-import Sorting from "./components/sorting";
+import ThumbUpTwoToneIcon from '@mui/icons-material/ThumbUpTwoTone';
+import Sorting from "./components/sorting.jsx";
+import ReportButton from "./components/reportbutton.jsx";
 
 export default function Home() {
   const [data, setData] = useState([]);
-
   const [selectedKeys, setSelectedKeys] = useState(new Set(["earliest"]));
 
   const selectedValue = useMemo(
@@ -29,7 +30,8 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-white dark:bg-black">
+    <div
+      className="flex flex-col items-center min-h-screen bg-white dark:bg-black">
       <main className="flex flex-col items-center pt-20">
         <Image
           src="/artery.png"
@@ -38,8 +40,10 @@ export default function Home() {
           height={120}
           priority
         />
-        <h1 className="mt-4 text-3xl font-bold text-black dark:text-white">KU Vein</h1>
-        <p className="mt-4 text-xl text-black dark:text-white">รีวิว แบ่งปัน Q&A</p>
+        <h1 className="mt-4 text-3xl font-bold text-black dark:text-white">KU
+          Vein</h1>
+        <p className="mt-4 text-xl text-black dark:text-white">รีวิว แบ่งปัน
+          Q&A</p>
       </main>
       <div className="mt-8 w-full max-w-6xl">
         <div className="inline-flex items-center justify-end text-black">
@@ -60,7 +64,34 @@ export default function Home() {
           className="w-full h-12 px-4 py-2 text-gray-700 rounded-md border border-gray-300 focus:outline-none focus:border-2"
         />
       </div>
-      <Sorting selectedKeys={selectedKeys} setSelectedKeys={setSelectedKeys} />
+      <Sorting selectedKeys={selectedKeys} setSelectedKeys={setSelectedKeys}/>
+      <div
+        className="w-full max-w-5xl text-black dark:text-white">
+        <fieldset className="border border-gray-300 rounded-md p-4">
+          <legend
+            className="p-2 border-solid border rounded text-black font-bold border-gray-300 dark:text-white">
+            1346012 | Bacon Ipsum 101
+          </legend>
+          <div className="text-black dark:text-white">
+            <p>Bacon ipsum dolor amet prosciutto buffalo corned beef beef ham
+              hock. Landjaeger sausage boudin bresaola andouille bacon turkey
+              pastrami buffalo short loin swine. Short ribs sirloin pork beef
+              cow pork chop bresaola swine. Swine sausage turducken hamburger
+              tongue shankle tenderloin porchetta picanha frankfurter short
+              ribs andouille ham hock bresaola alcatra.</p>
+            <br/>
+            <p className="text-gray-300 text-right">Ichi</p>
+            <hr/>
+            <div className="text-gray-300 flex justify-between">
+              <p className="text-left"><ThumbUpTwoToneIcon/></p>
+              <div className="text-right">
+                <ReportButton/>
+                <Button>Share</Button>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+      </div>
     </div>
   );
 }
