5fa703fe5aea4819fd1f805646d8e9d94ce18d8a
PanidaRumriankit <panida.rum@ku.th>
2024-10-15 20:30:19 +0700

    chore: modify user dropdown ui

--- frontend/src/app/components/userdropdown.jsx
+++ frontend/src/app/components/userdropdown.jsx
@@ -11,14 +11,15 @@ export default function UserDropdown() {
         <PersonIcon className="w-7 h-7 text-gray-200 hover:text-black hover:cursor-pointer" />
       </DropdownTrigger>
       <DropdownMenu
-        className="bg-white dark:bg-gray-800 shadow-lg rounded-md outline-none w-28 p-0"
+        className="bg-white dark:bg-zinc-800 shadow-lg rounded-md outline-none w-36 p-0"
       >
         {usersDropdown.map((item) => (
           <DropdownItem
             key={item.key}
-            className={`${
-              item.key === "logout" ? "text-red-300 hover:text-red-500 hover:bg-red-100 dark:hover:bg-red-700" : ""
-            } hover:bg-gray-100 dark:hover:bg-gray-700 rounded-md transition-colors hover:cursor-pointer px-2 py-1`}
+            className={`
+              ${item.key === "logout" ? "!text-red-300 hover:!text-red-500" : ""}
+              hover:bg-gray-100 dark:hover:bg-zinc-700 dark:text-white rounded-md transition-colors hover:cursor-pointer px-2 py-1
+            `}
           >
             {item.label}
           </DropdownItem>
