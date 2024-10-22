45a9fa0db36cf5c4303aab66098998fdf0a9ae98
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-22 11:42:52 +0700

    feat: Use OAuth token to do API authentication

--- backend/forms/api.py
+++ backend/forms/api.py
@@ -1,13 +1,25 @@
 """This module use for send the data from Django to Next.js."""
 
 from ninja_extra import NinjaExtraAPI
-from ninja_jwt.controller import NinjaJWTDefaultController
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from decouple import config
+
 from .db_management import DatabaseBackup
 from .db_query import DatabaseQuery
 
 app = NinjaExtraAPI()
-app.register_controllers(NinjaJWTDefaultController)
 
+
+def verify_google_token(token: str):
+    """Verify the Google OAuth token from the frontend."""
+    try:
+        is_valid = id_token.verify_oauth2_token(token, requests.Request(), config('GOOGLE_CLIENT_ID', cast=str,
+                        default='sif'))
+        return is_valid
+
+    except ValueError:
+        print(f"Status_code: 401 Invalid token")
 
 @app.get("/database/course_data")
 def database(request):
@@ -15,6 +27,18 @@ def database(request):
     print(request)
 
     return DatabaseQuery().send_all_course_data()
+
+@app.get("/database/cou")
+def test_auth(request):
+    """For test API authentication only."""
+    token = request.headers.get("Authorization").split(" ")[1]
+    email = request.json().get("email")
+
+    check_token = verify_google_token(token)
+
+    if check_token['email'] == email:
+        return DatabaseQuery().send_all_course_data()
+    return []
 
 
 def backup(request):
--- backend/kuvein/settings.py
+++ backend/kuvein/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ninja_extra',
-    'ninja_jwt',
 ]
 
 MIDDLEWARE = [
