bbe8a43dcdc50725b2a5fc6757ca5b3c3a056f3a
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-23 22:59:30 +0700

    feat: create FactoryMethod for PostStrategy

--- backend/forms/db_post.py
+++ backend/forms/db_post.py
@@ -24,6 +24,37 @@ class PostStrategy(ABC):
         pass
 
 class ReviewPost(PostStrategy):
+    """Class for created new CourseReview object."""
 
     def post_data(self, data):
+        """Add the data to the CourseReview."""
         CourseReview.objects.create(**data)
+
+class PostFactory:
+    """Factory class to handle query strategy selection."""
+
+    strategy_map = {
+        "review": ReviewPost
+    }
+
+    @classmethod
+    def get_query_strategy(cls, data, query: str):
+        """
+        Update the data based on the name of table.
+
+        Args:
+            data: The data for update value in database from frontend.
+            query (str): The query parameter to choose the strategy.
+
+        Returns:
+            QueryStrategy: The corresponding query strategy class.
+
+        Raises:
+            ValueError: If the query string doesn't match any available strategies.
+        """
+        query_lower = query.lower()
+        if query_lower in cls.strategy_map:
+            cls.strategy_map[query_lower](data)
+            return True
+        else:
+            return False
