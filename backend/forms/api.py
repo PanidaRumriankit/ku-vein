e2d856326eee1a55209a9a221fbe97690010f9f3
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-10-24 13:17:53 +0700

    chore: log an error message when Value error

--- backend/forms/api.py
+++ backend/forms/api.py
@@ -53,7 +53,7 @@ def get_sorted_data(request):
         return Response(strategy.get_data())
 
     except ValueError as e:
-        logger.error("Value boom boom")
+        logger.error(e)
         return Response({"error": str(e)}, status=400)
 
 @app.get("/database/cou")
