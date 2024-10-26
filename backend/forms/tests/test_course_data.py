cc6163075fe71b0e573abfcff9a3aef64f260af8
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-26 18:20:07 +0700

    feat: finish InterQueryTests

--- backend/forms/tests/test_course_data.py
+++ backend/forms/tests/test_course_data.py
@@ -1,10 +1,8 @@
 # pylint: skip-file
 # -1: [file-ignored]
 
-from ..models import CourseData
+from ..models import CourseData, Inter
 from ..db_query import DatabaseQuery
-from ..db_post import UserDataPost, ReviewPost
-from ..db_management import DatabaseManagement
 from django.test import TestCase
 
 
@@ -15,11 +13,59 @@ class InterQueryTests(TestCase):
         """Set up reusable instances for tests."""
         self.inter = DatabaseQuery()
 
-        data = {"course_id":"1", "faculty":"Miracle", "course_type":"Priest", "course_name":"Basic Miracle 1"}
-        CourseData.objects.create(**data)
+        self.course = []
+
+        test_data = [
+            {"course_id": "1", "faculty": "Miracle", "course_type": "Priest", "course_name": "Basic Miracle 1"},
+            {"course_id": "2", "faculty": "Sorcery", "course_type": "Sorcerer", "course_name": "Soul Arrow Mastery"},
+            {"course_id": "3", "faculty": "Pyromancy", "course_type": "Pyromancer", "course_name": "Flame Manipulation"},
+            {"course_id": "4", "faculty": "Hexes", "course_type": "Hexer", "course_name": "Dark Orb Fundamentals"},
+            {"course_id": "5", "faculty": "Faith", "course_type": "Knight", "course_name": "Sacred Oath"},
+        ]
+
+        for insert_data in test_data:
+            self.course.append(CourseData.objects.create(**insert_data))
+
+        for course in self.course:
+            Inter.objects.create(course=course)
 
     def test_correct_data_format(self):
         """Data should return as a list."""
-        print(CourseData.objects.all())
-        self.assertEqual(self.inter.send_all_course_data(), [])
+        self.assertIsInstance(self.inter.send_all_course_data(), list)
 
+    def test_correct_data_output(self):
+        """send_all_course_data() should return the course data."""
+
+        expected_data = [
+            {"course_id": "1", "faculty": "Miracle", "course_name": "Basic Miracle 1"},
+            {"course_id": "2", "faculty": "Sorcery", "course_name": "Soul Arrow Mastery"},
+            {"course_id": "3", "faculty": "Pyromancy","course_name": "Flame Manipulation"},
+            {"course_id": "4", "faculty": "Hexes", "course_name": "Dark Orb Fundamentals"},
+            {"course_id": "5", "faculty": "Faith", "course_name": "Sacred Oath"},
+        ]
+
+        # Extract the values from each dictionary in both expected and actual data
+        expected_values = [list(sorted(item.values())) for item in expected_data]
+        result_values = [list(sorted(item.values())) for item in self.inter.send_all_course_data()]
+
+        self.assertEqual(result_values, expected_values)
+
+
+class SpecialQueryTests(TestCase):
+    def setUp(self):
+        """Set up reusable instances for tests."""
+        pass
+
+    def test_correct_data_format(self):
+        """Data should return as a list."""
+        pass
+
+
+class NormalQueryTests(TestCase):
+    def setUp(self):
+        """Set up reusable instances for tests."""
+        pass
+
+    def test_correct_data_format(self):
+        """Data should return as a list."""
+        pass
