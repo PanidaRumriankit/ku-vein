8bb954fc45077c5fa31aba6eec9d50840d4e5b61
PanidaRumriankit <panida.rum@ku.th>
2024-11-02 18:07:08 +0700

    feat: refresh token for 1 hour added

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -20,19 +20,57 @@ export const authOptions = {
                 token.accessToken = account.access_token;
                 token.email = profile?.email;
                 token.idToken = account.id_token;
+                token.accessTokenExpires = Date.now() + account.expires_at * 1000;
             }
 
-            return token;
+            if (Date.now() < token.accessTokenExpires) {
+                return token;
+            }
+
+            console.log("Access token has expired, refreshing...");
+            return await refreshAccessToken(token);
         },
         async session({ session, token }) {
             session.email = token.email;
-            session.idToken = token.idToken
+            session.idToken = token.idToken;
+            session.accessToken = token.accessToken;
 
             return session;
         },
     },
 };
 
+async function refreshAccessToken(token) {
+    try {
+        const url = `https://oauth2.googleapis.com/token?client_id=${process.env.GOOGLE_CLIENT_ID}&client_secret=${process.env.GOOGLE_CLIENT_SECRET}&grant_type=refresh_token&refresh_token=${token.refreshToken}`;
+
+        const response = await fetch(url, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            method: "POST"
+        });
+
+        const refreshedTokens = await response.json();
+
+        if (!response.ok) throw refreshedTokens;
+
+        return {
+            ...token,
+            accessToken: refreshedTokens.access_token,
+            accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+            idToken: refreshedTokens.id_token ?? token.idToken
+        };
+    } catch (error) {
+        console.error("Error refreshing access token:", error);
+
+        return {
+            ...token,
+            error: "RefreshAccessTokenError"
+        };
+    }
+}
+
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
