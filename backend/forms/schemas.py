772c0e9f08dc16c063b49f1af3206ce11f0e2425
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-11-02 15:54:15 +0700

    feat: add new endpoint for add follower and add new api schemas for follower feature.

--- backend/forms/schemas.py
+++ backend/forms/schemas.py
@@ -76,6 +76,12 @@ class UserDataSchema(ModelSchema):
         fields = ['email']
 
 
+class FollowSchema(ModelSchema):
+    """Schema for follower feature."""
+    current_user_id: str
+    target_user_id: str
+
+
 class CourseReviewSchema(ModelSchema):
     """
     Schema for CourseReview model, representing reviews left by users.
