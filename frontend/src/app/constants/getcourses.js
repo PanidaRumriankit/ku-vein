ff3bcfd739f8da581dc09510250cc5356a9d3963
PanidaRumriankit <panida.rum@ku.th>
2024-11-02 15:09:28 +0700

    feat: get user by api

--- frontend/src/app/constants/getcourses.js
+++ frontend/src/app/constants/getcourses.js
@@ -1,7 +1,5 @@
 export default async function GetDjangoApiData() {
   try {
-    // get courses with type param
-    // TODO implement user to select the type of course
     const response = await fetch("http://127.0.0.1:8000/api/course");
     const responseData = await response.json();
     console.log('Received data from Django:', responseData);
--- frontend/src/app/profile/page.jsx
+++ frontend/src/app/profile/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSession } from "next-auth/react";
 import { HexColorPicker } from 'react-colorful';
 import BrushIcon from '@mui/icons-material/Brush';
@@ -9,16 +9,36 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
+import GetUserData from '../constants/getuser';
 
 export default function Profile() {
   const [activeTab, setActiveTab] = useState('posts');
   const {data: session} = useSession();
   const [hovered, setHovered] = useState(false);
   const [colorPickerOpen, setColorPickerOpen] = useState(false);
-  const [color, setColor] = useState('#000000');
-
-  const handleGradientClick = () => setColorPickerOpen(true);
+  const [patchData, setPatchData] = useState(null);
+  const handleColorClick = () => setColorPickerOpen(true);
   const closeColorPicker = () => setColorPickerOpen(false);
+
+  if (!session) return null;
+
+  useEffect(() => {
+    async function fetchData() {
+      const userData = await GetUserData(session.user.email);
+      console.log('userData: ', userData);
+      setPatchData({
+        id: userData.id,
+        username: userData.username,
+        desc: userData.desc,
+        pf_color: userData.pf_color,
+      });
+    }
+    fetchData();
+  }, [session]);
+
+  if (!patchData) return null;
+  
+  console.log('Patch Data:', patchData);
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-white dark:bg-black">
@@ -26,15 +46,15 @@ export default function Profile() {
       <div className="w-full p-6 rounded-md text-center text-black dark:text-white relative">
         {/* Profile Background */}
         <div
-          className="w-full h-48"
-          style={{ background: color }}
+          className="w-100 h-48 -mx-6"
+          style={{ background: patchData.pf_color }}
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
-          onClick={handleGradientClick}
+          onClick={handleColorClick}
         >
           {hovered && (
-            <div className="absolute w-full h-56 inset-0 bg-black bg-opacity-30 flex items-center justify-end pr-8 cursor-pointer">
-              <BrushIcon className="text-white text-3xl mt-40" />
+            <div className="w-full h-48 inset-0 bg-black bg-opacity-30 flex items-center justify-end pr-4 cursor-pointer">
+              <BrushIcon className="text-white text-3xl mt-36" />
             </div>
           )}
 
@@ -56,7 +76,7 @@ export default function Profile() {
         )}
 
         <div className="mt-16"> {/* Adjusted to move below profile pic */}
-          <h1 className="text-2xl font-semibold">Username</h1>
+          <h1 className="text-2xl font-semibold">{patchData.username}</h1>
           <p className="text-gray-600">UserID</p>
           <p className="text-gray-500">Description</p>
           <div className="flex justify-center space-x-4 mt-4">
@@ -71,11 +91,11 @@ export default function Profile() {
         <DialogTitle>Background Color</DialogTitle>
         <DialogContent>
           <div className="mt-4">
-            <HexColorPicker color={color} onChange={setColor} className='mx-auto' />
+            <HexColorPicker color={patchData.pf_color} onChange={setPatchData.pf_color} className='mx-auto' />
             <TextField
               fullWidth
-              value={color}
-              onChange={(e) => setColor(e.target.value)}
+              value={patchData.pf_color}
+              onChange={(e) => setPatchData(e.target.value)}
               variant="outlined"
               size="small"
               margin="dense"
@@ -86,7 +106,7 @@ export default function Profile() {
                       width: 24,
                       height: 24,
                       borderRadius: '50%',
-                      backgroundColor: color,
+                      backgroundColor: patchData.pf_color,
                       marginRight: 8,
                     }}
                   />
