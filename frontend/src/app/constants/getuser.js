83964030290269200aead7797498aca5424097dd
PanidaRumriankit <panida.rum@ku.th>
2024-11-09 22:03:47 +0700

    feat: getuser by id/email in one file

--- frontend/src/app/constants/getuser.js
+++ frontend/src/app/constants/getuser.js
@@ -1,6 +1,6 @@
-export default async function GetUserData(email) {
+export default async function GetUserData(params, type) {
   try {
-    const response = await fetch(`http://127.0.0.1:8000/api/user?email=${encodeURIComponent(email)}`);
+    const response = await fetch(`http://127.0.0.1:8000/api/user?${encodeURIComponent(type)}=${encodeURIComponent(params)}`);
     const responseData = await response.json();
     console.log('Received user data:', responseData);
     return responseData;
