088ab3247b84c9ed5193b43d831bb38874f03b62
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-11-10 16:48:03 +0700

    feat: change url path from local host to the deployed backend server

--- frontend/src/app/constants/getcourses.js
+++ frontend/src/app/constants/getcourses.js
@@ -1,6 +1,8 @@
+import {courseURL} from "./backurl.js";
+
 export default async function GetDjangoApiData() {
   try {
-    const response = await fetch("http://127.0.0.1:8000/api/course");
+    const response = await fetch(courseURL + `/course`);
     const responseData = await response.json();
     console.log('Received data from Django:', responseData);
     return responseData;
