fd3c6b07bef9fa70149ad463c8a43bffbff8f00f
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-11-22 22:01:16 +0700

    fix: check whether it is this line of code

--- frontend/src/app/constants/backurl.js
+++ frontend/src/app/constants/backurl.js
@@ -1,6 +1,6 @@
 // localhost use http://127.0.0.1:8000/api
 // deploy use https://ku-vein.onrender.com/api
-const backURL = 'https://ku-vein.onrender.com/api';
+const backURL = 'http://127.0.0.1:8000/api';
 const reviewURL = backURL + '/review';
 const userURL = backURL + '/user';
 const courseURL = backURL + '/course';
