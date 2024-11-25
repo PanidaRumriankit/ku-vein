2a8890920dad7e80a8a9d83960ad61f2ca2f20d2
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-11-25 16:23:21 +0700

    style, fix: remove download button and left only preview one

--- frontend/src/app/components/notebox.jsx
+++ frontend/src/app/components/notebox.jsx
@@ -1,3 +1,5 @@
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
 const pdfDataExample = {
   courses_id: "1",
   courses_name: "Wow",
@@ -14,13 +16,6 @@ export default function NoteBox({data}) {
   const pdfName = data['pdf_name'];
   const pdfURL = data['pdf_url'];
 
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = pdfURL;
-    link.download = pdfName;
-    link.click()
-  };
-
   return (
     <div className="border p-4 rounded-md shadow-md bg-white">
       {/* info */}
@@ -29,6 +24,7 @@ export default function NoteBox({data}) {
       <p><strong>
         โดย:</strong> {data.name} ({data.is_anonymous ? "Anonymous" : "Public"})
       </p>
+      <p><strong>ชื่อไฟล์:</strong> {pdfName}</p>
 
       <div className="flex gap-4 mt-4">
         {/* view */}
@@ -38,16 +34,9 @@ export default function NoteBox({data}) {
           rel="noopener noreferrer"
           className="px-4 py-2 bg-blue-500 text-white rounded-md shadow hover:bg-blue-600"
         >
-          ดู PDF
+          <VisibilityIcon/> PDF
         </a>
 
-        {/* download */}
-        <button
-          onClick={handleDownload}
-          className="px-4 py-2 bg-green-500 text-white rounded-md shadow hover:bg-green-600"
-        >
-          ดาวน์โหลด PDF
-        </button>
       </div>
     </div>
   )
