a3be201f677eca2c77761c93e1fee57a9cd10c39
PanidaRumriankit <panida.rum@ku.th>
2024-11-25 14:40:11 +0700

    fix: delete unused feature

--- frontend/src/app/constants/index.js
+++ frontend/src/app/constants/index.js
@@ -1,46 +1,3 @@
-const notifications = [
-  {
-    id: 1,
-    title: "Notification 1",
-    description: "This is the first notification",
-    type: "success",
-    duration: 5000,
-    isRead: true,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 2,
-    title: "Notification 2",
-    description: "This is the second notification",
-    type: "error",
-    duration: 5000,
-    isRead: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 3,
-    title: "Notification 3",
-    description: "This is the third notification",
-    type: "info",
-    duration: 5000,
-    isRead: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 4,
-    title: "Notification 4",
-    description: "This is the fourth notification",
-    type: "warning",
-    duration: 5000,
-    isRead: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
-
 const usersDropdown = [
   {
     key: "profile",
@@ -133,7 +90,6 @@ const gradeColors = {
 };
 
 export {
-  notifications,
   usersDropdown,
   colorPallet,
   sortOption,
