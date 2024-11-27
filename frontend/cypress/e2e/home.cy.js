9a0645aabd0a6d9b5957c3e8b7dccd12c467061c
PanidaRumriankit <panida.rum@ku.th>
2024-11-28 01:20:16 +0700

    feat: add test earliest sorting

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -1,16 +1,169 @@
 describe('Home Page', () => {
   beforeEach(() => {
+    // GET courses
     cy.intercept('GET', '**/api/course', {
       fixture: 'courses.json'
     }).as('getCourses');
 
+    // GET latest reviews
+    cy.intercept('GET', '**/api/review?sort=latest**', {
+      fixture: 'reviews_latest.json'
+    }).as('getReviewsLatest');
+
+    // GET earliest reviews
+    cy.intercept('GET', '**/api/review?sort=earliest**', {
+      fixture: 'reviews_earliest.json'
+    }).as('getReviewsEarliest');
+
+    // GET upvoted Reviews
+    cy.intercept('GET', '**/api/review?sort=upvote**', {
+      fixture: 'reviews_upvote.json'
+    }).as('getReviewsUpvote');
+
+    // POST upvote
+    cy.intercept('POST', '**/api/upvote*', {
+      statusCode: 200,
+      body: { success: true }
+    }).as('upvoteReview');
+
+    // GET upvote
+    cy.intercept('GET', '**/api/upvote*', {
+      statusCode: 200,
+      body: { voted: false }
+    }).as('checkUpvote');
+
     cy.visit('/');
   });
 
+  // Check main elements
   it('should display the header and main elements', () => {
     cy.get('h1').contains('KU Vein');
     cy.get('p').contains('รีวิว แบ่งปัน Q&A');
     cy.get('img[alt="Artery"]').should('be.visible');
+  });
+
+  // Check if the sorting dropdown is visible
+  it('renders the sorting dropdown', () => {
+    cy.get('button')
+      .contains('latest')
+      .should('be.visible');
+  });
+
+  // Check if the review cards contain correct information
+  it('renders review cards with correct information', () => {
+    cy.wait('@getReviewsLatest');
+
+    cy.get('fieldset').each(($card, index) => {
+      cy.wrap($card)
+        .find('legend')
+        .should('contain', ['01200432-00 | Rolling Stock Technology', '01200101-64 | Innovative Thinking'][index]);
+
+      cy.wrap($card)
+        .find('[class*="MuiRating"]')
+        .should('exist');
+
+      cy.wrap($card)
+        .contains('p', ['A', 'Yes I like itttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt'][index])
+        .should('be.visible');
+
+      cy.wrap($card)
+        .contains('p', ['ผู้สอน: A', 'ผู้สอน: Kasidet'][index])
+        .should('be.visible');
+
+      cy.wrap($card)
+        .contains('p', ['เกรด: C', 'เกรด: A'][index])
+        .should('be.visible');
+
+      cy.wrap($card)
+      .contains('p', ['26 พ.ย. 2567 โดย: user_2', '26 พ.ย. 2567 โดย: roblox'][index])
+      .should('be.visible');
+
+      cy.wrap($card)
+        .find('button')
+        .contains((['0', '10'][index]))
+        .should('be.visible');
+    });
+  });
+
+  // it('tests upvote functionality', () => {
+  //   // Simulate user login (mock session)
+  //   cy.window().then((win) => {
+  //     win.localStorage.setItem('session', JSON.stringify({
+  //       email: 'test@example.com',
+  //       idToken: 'mock-token'
+  //     }));
+  //   });
+
+  //   // Reload to apply mocked session
+  //   cy.reload();
+
+  //   // Wait for reviews to load
+  //   cy.wait('@getReviews');
+
+  //   // Find first upvote button and click
+  //   cy.get('button').contains('0')
+  //     .click()
+  //     .wait('@upvoteReview');
+
+  //   cy.get('button')
+  //     .contains('1')
+  //     .should('be.visible');
+  // });\
+
+  // Check if earliest sorting works
+  it('checks earliest sorting affects review order', () => {
+    
+    cy.get('button')
+      .contains('latest')
+      .click();
+    
+    // Open sorting dropdown
+    cy.get('button')
+      .contains('latest')
+      .click();
+
+    // Select earliest sorting option
+    cy.get('[role="dialog"]')
+      .find('[role="menuitemradio"]')
+      .contains('earliest')
+      .click();
+    
+    cy.wait('@getReviewsEarliest');
+
+    cy.get('fieldset').then(($cards) => {
+      const reversedCards = Cypress.$.makeArray($cards).reverse();
+
+      Cypress._.each(reversedCards, ($card, index) => {
+      cy.wrap($card)
+        .find('legend')
+        .should('contain', ['01200432-00 | Rolling Stock Technology', '01200101-64 | Innovative Thinking'][index]);
+
+      cy.wrap($card)
+        .find('[class*="MuiRating"]')
+        .should('exist');
+
+      cy.wrap($card)
+        .contains('p', ['A', 'Yes I like itttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt'][index])
+        .should('be.visible');
+
+      cy.wrap($card)
+        .contains('p', ['ผู้สอน: A', 'ผู้สอน: Kasidet'][index])
+        .should('be.visible');
+
+      cy.wrap($card)
+        .contains('p', ['เกรด: C', 'เกรด: A'][index])
+        .should('be.visible');
+
+      cy.wrap($card)
+      .contains('p', ['26 พ.ย. 2567 โดย: user_2', '26 พ.ย. 2567 โดย: roblox'][index])
+      .should('be.visible');
+
+      cy.wrap($card)
+        .find('button')
+        .contains((['0', '10'][index]))
+        .should('be.visible');
+      });
+    });
   });
 
   it('should find the Search input', () => {
