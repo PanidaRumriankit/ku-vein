ad491d44d5b9dd6b72d2c8d405009240f79c6313
PanidaRumriankit <panida.rum@ku.th>
2024-11-28 21:47:38 +0700

    featL test if login successfully

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -5,11 +5,26 @@ describe('Layout component', () => {
       req.reply({
         status: 'authenticated',
         user: {
-          email: 'test@ku.ac.th',
+          email: 'test@ku.th',
           name: 'Test User'
-        }
+        },
+        email: 'test@ku.th',
+        idToken: 'mock-id-token'
       })
     }).as('session');
+
+    // GET courses
+    cy.intercept('GET', '**/api/course', {
+      fixture: 'courses.json'
+    }).as('getCourses');
+
+    // Mock the review submission API
+    cy.intercept('POST', '**/api/review', (req) => {
+      req.reply({ 
+        statusCode: 200, 
+        body: { message: 'Review added successfully' } 
+      })
+    }).as('submitReview')
 
     cy.visit('/')
   })
@@ -18,16 +33,44 @@ describe('Layout component', () => {
   it('can login', () => {
     cy.wait('@session')
       .its('response.body.user.email')
-      .should('equal', 'test@ku.ac.th');
+      .should('equal', 'test@ku.th');
   })
 
   it('renders correctly when authenticated', () => {
-    // Wait for session and user creation
-    cy.wait('@session')
-
     // Check for key authenticated elements
     cy.contains('KU Vein').should('be.visible')
     cy.get('[data-testid="PersonIcon"]').should('exist')
     cy.contains('© 2024 KU Vein. All rights reserved').should('be.visible')
+  })
+
+  it('authenticated users cannot add reviews if data is not filled', () => {
+    // Check for AddIcon and click it
+    cy.get('svg[data-testid="AddIcon"]').should('exist');
+    cy.get('svg[data-testid="AddIcon"]').click();
+    // Check if submit button is disabled
+    cy.contains('Submit').should('be.disabled');
+  })
+
+  it('allows selecting course and faculty', () => {
+    // Open review popup
+    cy.get('button')
+      .find('svg[data-testid="AddIcon"]')
+      .parent()
+      .click();
+
+    // Mock search components
+    cy.get('[id="react-select-3-input"]').type('01200101-64', {delay: 100});
+    cy.wait('@getCourses');
+    cy.get('body')
+      .find('div[role="option"]')
+      .first()
+      .click({ force: true });
+    
+    // Mock faculty selection
+    cy.get('[id="react-select-4-input"]').type('คณะวิศวกรรมศาสตร์', {delay: 1000});
+    cy.get('body')
+      .find('div[role="option"]')
+      .first()
+      .click({ force: true });
   })
 })
