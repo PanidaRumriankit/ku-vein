72f10c35d63cd4f55a2fab4feb92c18eb1867cb7
PanidaRumriankit <panida.rum@ku.th>
2024-12-02 16:43:36 +0700

    feat: qanda_id page

--- frontend/src/app/components/questiontext.jsx
+++ frontend/src/app/components/questiontext.jsx
@@ -3,34 +3,48 @@
 import {useRouter, usePathname} from "next/navigation";
 import {useEffect, useState} from "react";
 import {useSession} from "next-auth/react";
-import ReportButton from "./reportbutton.jsx";
 import ShareButton from "./sharebutton.jsx";
 import TurnedInIcon from '@mui/icons-material/TurnedIn';
 import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
 import ThumbUpTwoToneIcon from '@mui/icons-material/ThumbUpTwoTone';
-import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
-import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
-import Button from "@mui/material/Button";
+import {Button} from "@nextui-org/button";
+import PopupProfile from "./popupprofile.jsx";
+import { questionURL } from "../constants/backurl.js";
+import BookmarkButton from "./bookmarkbutton.jsx";
+import GetUser from "../constants/getuser";
 
-
-export default function QuestionText({item}) {
+export default function QuestionText({ item, bookmark }) {
   const router = useRouter();
   const pathname = usePathname();
   const {data: session} = useSession();
   const [upvoteCount, setUpvoteCount] = useState(0);
   const [isVoted, setIsVoted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [popupPosition, setPopupPosition] = useState({ x: 0, y: 0 });
+  const [formattedDate, setFormattedDate] = useState("");
   // const [selectedKeys, setSelectedKeys] = useState(new Set(["latest"]));
-  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [userId, setUserId] = useState(null);
   const idToken = session?.idToken || session?.accessToken;
   const email = session?.email;
 
+  const fetchUser = async () => {
+    const response = await GetUser(email, "email");
+    setUserId(response.id);
+  };
+
+  useEffect(() => {
+    if (session) {
+      fetchUser();
+    }
+  }, [session]);
+
   const handleUpvote = async () => {
-    if (isLoading || !email || !idToken) return;
+    if (isLoading || !session) return;
     setIsLoading(true);
 
     try {
-      const response = await fetch(upvoteURL, {
+      const response = await fetch(questionURL + '/upvote', {
         method: "POST",
         headers: {
           "Authorization": `Bearer ${idToken}`,
@@ -38,8 +52,8 @@ export default function QuestionText({item}) {
           "email": email,
         },
         body: JSON.stringify({
-          review_id: item.reviews_id,
-          email: email
+          question_id: String(item.questions_id),
+          user_id: String(userId)
         })
       });
 
@@ -56,41 +70,94 @@ export default function QuestionText({item}) {
     }
   };
 
+  const handleMouseEnter = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    setPopupPosition({
+      x: rect.left + window.scrollX,
+      y: rect.bottom + window.scrollY + 10,
+    });
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  useEffect(() => {
+    if (item.post_time) {
+      const formatted = new Date(item.post_time).toLocaleDateString("th-TH", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      setFormattedDate(formatted);
+    }
+  }, [item.post_time]);
+
   return (
     <div className="mx-auto my-4 w-[32rem] max-w-4xl text-black dark:text-white">
       <fieldset
         className="border border-2 border-blue-300 rounded-md p-6 w-full bg-white dark:bg-gray-800 shadow-xl"
       >
           <div className="flex justify-between">
-            <h3 className="text-xl font-semibold text-[#44b3ab] break-all">{item.title}</h3>
-            <div className="ml-4 cursor-pointer hover:scale-105" onClick={(e) => {setIsBookmarked(!isBookmarked)}}>
+            <h3 className="text-xl font-semibold text-[#44b3ab] break-all">{item.questions_title}</h3>
+            {/* <div className="ml-4 cursor-pointer hover:scale-105" onClick={(e) => {setIsBookmarked(!isBookmarked)}}>
               {isBookmarked ? <TurnedInIcon /> : <TurnedInNotIcon />}
-            </div>
+            </div> */}
           </div>
           <br/>
-          <p className="text-lg mb-4">{item.description}</p>
+          <p className="text-lg mb-4">{item.questions_text}</p>
           <div
             className="flex items-center justify-between text-gray-300 text-right">
             <p className="text-right">
-              {new Date(item.createdAt).toLocaleString()}
+              {formattedDate}
             </p>
-            <p className="text-left">
-              โดย: {item.name || item.username}
-            </p>
+            <p
+            className={!item.is_anonymous ? "cursor-pointer": ""}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={(e) => {
+              if (!item.anonymous) {
+                e.stopPropagation()
+                router.push(`/user/${item.users.toString()}`);}
+            }}
+          >
+            โดย: {item.pen_names}
+          </p>
+          {!item.anonymous && isHovered && (
+            <div
+              style={{
+                position: "absolute",
+                top: popupPosition.y,
+                left: popupPosition.x,
+                width: "14vw",
+                height: "8vw",
+                zIndex: 1000,
+                border: "1px solid #ccc",
+                background: theme === 'dark' ? '#000' : '#fff',
+                boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <PopupProfile userId={item.users.toString()}/>
+            </div>
+          )}
           </div>
           <hr/>
-          <div className="text-gray-300 flex justify-between mt-2 -mb-4">
-            <div className="text-left text-2xl flex space-x-4">
+          <div className="text-gray-300 flex justify-between -ml-4 mt-2 -mb-4">
+            <div className="text-left text-2xl flex space-x-4" onClick={(e) => {e.stopPropagation()}}>
               <Button variant="light" onClick={handleUpvote}
                       disabled={!session || isLoading}>
-                <ThumbUpTwoToneIcon/>    {upvoteCount}
+                <ThumbUpTwoToneIcon
+                  color={isVoted ? "primary" : ""}/> {upvoteCount}
               </Button>
             </div>
-            <div className="text-right" >
-              <ReportButton/>
-              <ShareButton/>
-            </div>
+          <div className="text-right" onClick={(e) => e.stopPropagation()}>
+            <ShareButton/>
+            <BookmarkButton id={item.questions_id} type="qa" bookmark={bookmark}/>
           </div>
+        </div>
       </fieldset>
     </div>
   );
--- frontend/src/app/course/[id]/qanda/[qanda_id]/page.jsx
+++ frontend/src/app/course/[id]/qanda/[qanda_id]/page.jsx
@@ -2,31 +2,114 @@
 
 import {useEffect, useState} from "react";
 import {useParams, useRouter} from "next/navigation";
-import {question, answer} from "../../../../constants/index";
+import {useSession} from "next-auth/react";
+import {question} from "../../../../constants/index";
 import QuestionCard from "../../../../components/questioncard";
 import AnswerCard from "../../../../components/answercard";
 import QuestionText from "../../../../components/questiontext";
 import CourseNavigationBar from "../../../../components/coursenavigation";
 import Sorting from "../../../../components/sorting";
 import AddAnswer from "../../../../components/addanswer";
+import backurl from "../../../../constants/backurl";
+import GetQuestion from "../../../../constants/getquestion";
+import GetBookmarks from "../../../../constants/getbookmarks";
 
 export default function EachQuestionPage() {
   const router = useRouter();
   const params = useParams();
+  const {data: session} = useSession();
   const questionId = parseInt(params.qanda_id, 10);
-  const [selectedKeys, setSelectedKeys] = useState(new Set(["latest"]));
+  const [selectedKeys, setSelectedKeys] = useState("latest");
   const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
+  const [bookmarkQuestion, setBookmarkQuestion] = useState([]);
+  const [isBookmarked, setIsBookmarked] = useState(false);
 
-  console.log('questionID:', questionId);
+  // console.log('questionID:', questionId);
+
+  const fetchBookmarks = async () => {
+    const response = await GetBookmarks(session.email);
+    setBookmarkQuestion(response.filter((bookmark) => bookmark.data_type === "qa"));
+    console.log('Received bookmarks questions:', bookmarkQuestion);
+  };
+
+  useEffect(() => {
+    if (session) {
+      fetchBookmarks();
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (bookmarkQuestion.length > 0) {
+      const isBookmark = bookmarkQuestion.some((bookmark) => bookmark.object_id === questionId);
+      setIsBookmarked(isBookmark);
+    }
+  }, [bookmarkQuestion, questionId]);
+
+  useEffect(() => {
+    const fetchAnswers = async () => {
+      try {
+        const response = await fetch(`${backurl}/qa?question_id=${questionId}&mode=${selectedKeys}`);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch answers: ${response.statusText}`);
+        }
+        const data = await response.json();
+        setAnswers(data);
+      } catch (error) {
+        console.error("Error fetching answers:", error);
+      }
+    };
+
+    fetchAnswers();
+  }, [questionId, selectedKeys]);
+
+  // console.log("QuestionText Component:", QuestionText);
 
   // useEffect(() => {
   //   const fetchQuestions = async () => {
-  //     const data = await GetQuestion(courseId);
-  //     setQuestions(data);
+  //     try {
+  //       console.log("Fetching questions...");
+  //       const response = await fetch('https://ku-vein.onrender.com/api/qa?mode=' + selectedKeys);
+  //       console.log("Response received:", response);
+  //       if (!response.ok) {
+  //         console.error(`Failed to fetch. Status: ${response.status}`);
+  //         return;
+  //       }
+  //       const data = await response.json();
+  //       console.log("Parsed data:", data);
+  //       setQuestions(data);
+  //     } catch (error) {
+  //       console.error("Fetch error:", error);
+  //     }
   //   };
-
+  
   //   fetchQuestions();
-  // }, [courseId]);
+  // }, []);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const data = await GetQuestion(selectedKeys);
+      setQuestions(data);
+    };
+    fetchQuestions()
+  }, [selectedKeys]);
+
+  // useEffect(() => {
+  //   if (answers) {
+  //     console.log('answers:', answers);
+  //   }
+  // })
+
+  useEffect(() => {
+    if (questions) {
+      console.log('questions[questionId]:', questions[questionId]);
+    }
+  })
+  
+  // console.log('questions:', questions);
+  // console.log('questionId:', questionId);
+
+  // console.log('questions:', questions);
 
   return (
     <div className="text-black flex flex-col items-center
@@ -39,18 +122,22 @@ export default function EachQuestionPage() {
           </div>
       </div>
       <div className="flex flex-col items-center w-full max-w-5xl">
-        <QuestionText item={question[questionId - 1]}/>
+      {questions[questionId] ? (
+        <QuestionText item={questions[questionId]} bookmark={isBookmarked}/>
+      ) : (
+        <p>Loading question...</p>
+      )}
         <div className="flex w-1/2 justify-start font-bold my-4">
           <h1>Answers</h1>
         </div>
-        {answer.length > 0 ? (
-          answer.map((item, index) => (
+        {/* {answers.length > 0 ? (
+          answers.map((item, index) => (
             <AnswerCard item={item} key={index}/>
           ))
         ) : (
           <p className="text-green-400 text-center">No Q&A currently</p>
         )}
-        <AddAnswer />
+        <AddAnswer /> */}
       </div>
     </div>
   );
