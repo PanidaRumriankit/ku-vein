413ccb4f15f0c85a4bbf71198c687d346152c326
PanidaRumriankit <panida.rum@ku.th>
2024-12-02 23:45:32 +0700

    fix: change how getquestion works since backend have changed the endpoint

--- frontend/src/app/constants/getquestion.js
+++ frontend/src/app/constants/getquestion.js
@@ -1,8 +1,8 @@
 import {questionURL} from "./backurl.js";
 
-export default async function GetQuestion(sort) {
+export default async function GetQuestion(sort, id) {
   // Construct the URL with the sort parameter
-  const response = await fetch(questionURL + `?mode=${encodeURIComponent(sort)}`);
+  const response = await fetch(questionURL + `?course_id=${encodeURIComponent(id)}&mode=${encodeURIComponent(sort)}`);
 
   if (response.ok) {
     const data = await response.json();
