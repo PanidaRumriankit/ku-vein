5c40dcc19cc9b2044833aeafcc5e8ed09adea9a2
PanidaRumriankit <panida.rum@ku.th>
2024-12-02 20:09:56 +0700

    feat: modify answercard template

--- frontend/src/app/components/answercard.jsx
+++ frontend/src/app/components/answercard.jsx
@@ -3,15 +3,12 @@
 import {useRouter, usePathname} from "next/navigation";
 import {useEffect, useState} from "react";
 import {useSession} from "next-auth/react";
-import ReportButton from "./reportbutton.jsx";
 import ShareButton from "./sharebutton.jsx";
-import TurnedInIcon from '@mui/icons-material/TurnedIn';
-import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
 import ThumbUpTwoToneIcon from '@mui/icons-material/ThumbUpTwoTone';
-import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
-import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
-import Button from "@mui/material/Button";
-
+import {Button} from "@nextui-org/button";
+import { questionURL } from "../constants/backurl.js";
+import PopupProfile from "./popupprofile.jsx";
+import GetUser from "../constants/getuser";
 
 export default function AnswerCard({item}) {
   const router = useRouter();
@@ -20,17 +17,31 @@ export default function AnswerCard({item}) {
   const [upvoteCount, setUpvoteCount] = useState(item.upvote || 0);
   const [isVoted, setIsVoted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  // const [selectedKeys, setSelectedKeys] = useState(new Set(["latest"]));
-  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [popupPosition, setPopupPosition] = useState({ x: 0, y: 0 });
+  const [formattedDate, setFormattedDate] = useState("");
+  // const [selectedKeys, setSelectedKeys] = useState("latest");
+  const [userId, setUserId] = useState(null);
   const idToken = session?.idToken || session?.accessToken;
   const email = session?.email;
 
+  const fetchUser = async () => {
+    const response = await GetUser(email, "email");
+    setUserId(response.id);
+  };
+
+  useEffect(() => {
+    if (session) {
+      fetchUser();
+    }
+  }, [session]);
+
   const handleUpvote = async () => {
-    if (isLoading || !email || !idToken) return;
+    if (isLoading || !session) return;
     setIsLoading(true);
 
     try {
-      const response = await fetch(upvoteURL, {
+      const response = await fetch(questionURL + '/answer/upvote', {
         method: "POST",
         headers: {
           "Authorization": `Bearer ${idToken}`,
@@ -38,8 +49,8 @@ export default function AnswerCard({item}) {
           "email": email,
         },
         body: JSON.stringify({
-          review_id: item.reviews_id,
-          email: email
+          answer_id: String(item.answers_id),
+          user_id: String(userId)
         })
       });
 
@@ -50,11 +61,37 @@ export default function AnswerCard({item}) {
         console.log("Error upvoting:", await response.text());
       }
     } catch (error) {
-      console.error("Error upvoting review:", error);
+      console.error("Error upvoting answer:", error);
     } finally {
       setIsLoading(false);
     }
   };
+
+  const handleMouseEnter = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    setPopupPosition({
+      x: rect.left + window.scrollX,
+      y: rect.bottom + window.scrollY + 10,
+    });
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  useEffect(() => {
+    if (item.post_time) {
+      const formatted = new Date(item.post_time).toLocaleDateString("th-TH", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      setFormattedDate(formatted);
+    }
+  }, [item.post_time]);
 
   return (
     <div className="mx-auto my-4 w-[32rem] max-w-4xl text-black dark:text-white">
@@ -68,16 +105,42 @@ export default function AnswerCard({item}) {
             </div>
           </div> */}
           {/* <br/> */}
-          <p className="text-lg">{item.description}</p>
+          <p className="text-lg">{item.text}</p>
           <br/>
           <div
             className="flex items-center justify-between text-gray-300 text-right">
             <p className="text-right">
-              {new Date(item.createdAt).toLocaleString()}
+              {formattedDate}
             </p>
-            <p className="text-left">
-              โดย: {item.name || item.username}
-            </p>
+            <p
+            className={!item.anonymous ? "cursor-pointer": ""}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={(e) => {
+              if (!item.anonymous) {
+                e.stopPropagation()
+                router.push(`/user/${item.users.toString()}`);}
+            }}
+          >
+            โดย: {item.pen_names}
+          </p>
+          {!item.anonymous && isHovered && (
+            <div
+              style={{
+                position: "absolute",
+                top: popupPosition.y,
+                left: popupPosition.x,
+                width: "14vw",
+                height: "8vw",
+                zIndex: 1000,
+                border: "1px solid #ccc",
+                background: theme === 'dark' ? '#000' : '#fff',
+                boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <PopupProfile userId={item.users.toString()}/>
+            </div>
+          )}
           </div>
           <hr/>
           <div className="text-gray-300 flex justify-between mt-2 -mb-4">
@@ -88,7 +151,6 @@ export default function AnswerCard({item}) {
               </Button>
             </div>
             <div className="text-right">
-              <ReportButton/>
               <ShareButton/>
             </div>
           </div>
